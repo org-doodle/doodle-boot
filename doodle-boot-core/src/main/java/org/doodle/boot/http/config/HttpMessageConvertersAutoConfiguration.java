f145e333cf1909fb127f1b5eee256d8d47032638
TingyanShen <tingyan.shen@gmail.com>
2022-12-25 10:35:22 +0800

    Adding protobuf encoder/decoder for spring web and rsocket

---
+++ doodle-boot-core/src/main/java/org/doodle/boot/http/config/HttpMessageConvertersAutoConfiguration.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022-present Doodle. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.doodle.boot.http.config;
+
+import com.google.protobuf.Message;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+
+@SuppressWarnings("unused")
+@AutoConfiguration(
+    before =
+        org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration.class)
+@ConditionalOnClass({Message.class, ProtobufEncoder.class, ProtobufDecoder.class})
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class HttpMessageConvertersAutoConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public HttpMessageConverter<Message> protobufHttpMessageConverter() {
+    return new ProtobufHttpMessageConverter();
+  }
+}
